c82fc0d7075588661cb019fb3e69fd47d0dac933
PratikHan <patrick.hanchate@gmail.com>
2019-09-23 20:26:06 -0400

    Implemented Netwoerk api

---
+++ app/src/main/java/com/gotenna/mapboxdemo/Data/remote/RetrofitService.java
@@ -0,0 +1,23 @@
+package com.gotenna.mapboxdemo.Data.remote;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitService {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://annetog.gotenna.com";
+
+    public static Retrofit getRetrofitInstance(){
+
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+
+
+    }
+}
--- app/src/main/java/com/gotenna/mapboxdemo/repository/UserRepository.java
+++ app/src/main/java/com/gotenna/mapboxdemo/repository/UserRepository.java
@@ -1,6 +1,43 @@
 package com.gotenna.mapboxdemo.repository;
 
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import com.gotenna.mapboxdemo.Data.local.UserDatabase;
+import com.gotenna.mapboxdemo.Data.local.Users;
+import com.gotenna.mapboxdemo.Data.local.UsersDao;
+
+import java.util.List;
+
 public class UserRepository {
 
 
+    private static final String TAG = "UserRepository";
+    private UsersDao usersDao;
+    LiveData<List<Users>> allusers;
+
+    public UserRepository (Application application){
+        UserDatabase userDatabase = UserDatabase.getInstance(application);
+        usersDao = userDatabase.usersDao();
+        allusers = usersDao.getAllUsersByLat();
+    }
+
+    public void insert (Users users) {}
+
+
+    private static class InsertUserAsyncTask extends AsyncTask<Users, Void, Void>{
+
+        UsersDao usersDao;
+
+        public InsertUserAsyncTask(UsersDao usersDao) {this.usersDao=usersDao;};
+
+        @Override
+        protected Void doInBackground(Users... users) {
+            usersDao.insert(users[0]);
+            return null;
+        }
+    }
 }
+
