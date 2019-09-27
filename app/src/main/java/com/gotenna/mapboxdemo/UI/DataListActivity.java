f06f804a2e1d6329e315f368a8f92fddeba579b3
PratikHan <patrick.hanchate@gmail.com>
2019-09-27 19:46:15 -0400

    added connectivity exceptions

--- app/src/main/java/com/gotenna/mapboxdemo/UI/DataListActivity.java
+++ app/src/main/java/com/gotenna/mapboxdemo/UI/DataListActivity.java
@@ -3,8 +3,11 @@ package com.gotenna.mapboxdemo.UI;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.gotenna.mapboxdemo.Data.local.Users;
 import com.gotenna.mapboxdemo.Debug.Loggers;
@@ -22,14 +25,23 @@ public class DataListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_list);
 
+        RecyclerView recyclerView = findViewById(R.id.recyclerView1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final DataAdapter adapter = new DataAdapter();
+        recyclerView.setAdapter(adapter);
+
+
 
         userViewModel= ViewModelProviders.of(this).get(UserViewModel.class);
         userViewModel.getAllUsersData().observe(this, new Observer<List<Users>>() {
             @Override
             public void onChanged(List<Users> users) {
-                Loggers.show("DATALIST","ONCHANGED",""+users.get(0).getName());
+
+                adapter.setUserData(users);
             }
         });
-
+        
     }
 }
