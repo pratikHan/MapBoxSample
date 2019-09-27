bd4ef590c995fde484e5b79e87eb6d3f94729e2c
PratikHan <patrick.hanchate@gmail.com>
2019-09-26 22:35:34 -0400

    tested viewmodel data to datalistActivity

--- app/src/main/java/com/gotenna/mapboxdemo/UI/DataListActivity.java
+++ app/src/main/java/com/gotenna/mapboxdemo/UI/DataListActivity.java
@@ -1,18 +1,35 @@
 package com.gotenna.mapboxdemo.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
+import com.gotenna.mapboxdemo.Data.local.Users;
+import com.gotenna.mapboxdemo.Debug.Loggers;
 import com.gotenna.mapboxdemo.R;
+import com.gotenna.mapboxdemo.ViewModel.UserViewModel;
+
+import java.util.List;
 
 public class DataListActivity extends AppCompatActivity {
+
+    private UserViewModel userViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_list);
 
+
+        userViewModel= ViewModelProviders.of(this).get(UserViewModel.class);
+        userViewModel.getAllUsersData().observe(this, new Observer<List<Users>>() {
+            @Override
+            public void onChanged(List<Users> users) {
+                Loggers.show("DATALIST","ONCHANGED",""+users.get(0).getName());
+            }
+        });
 
     }
 }
